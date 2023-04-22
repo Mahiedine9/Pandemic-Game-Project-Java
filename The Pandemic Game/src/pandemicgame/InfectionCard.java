332fcd7de68a69a90765e61582ab9ca00d339d77
mahiedine.ferdjoukh.etu <mahieddine.ferdjoukh9@gmail.com>
2023-04-22 16:55:35 +0200

    mise a jour

--- The Pandemic Game/src/pandemicgame/InfectionCard.java
+++ The Pandemic Game/src/pandemicgame/InfectionCard.java
@@ -2,5 +2,18 @@ package pandemicgame;
 
 
 public class InfectionCard implements Card {
+	private City city;
+	
+	public InfectionCard(City city, Disease disease){
+		this.city = city;
+		this.disease = disease;
+			
+	}
+	public City GetCity() {
+		return this.city;
+	}
+	public Disease GetDisease() {
+		return this.disease();
+	}
 
 }
--- The Pandemic Game/src/pandemicgame/PlayerCard.java
+++ The Pandemic Game/src/pandemicgame/PlayerCard.java
@@ -1,6 +1,19 @@
 package pandemicgame;
-
-
 public class PlayerCard implements Card {
-
+	private City city
+	private string color;
+	
+	public PlayerCard(City city,string color ) {
+		this.city = city;
+		this.color = color;
+	}
+	public GetCity() {
+		return this.city;
+	}
+	public GetColor() {
+		return this.color;
+	}
+	
+	
+	
 }
