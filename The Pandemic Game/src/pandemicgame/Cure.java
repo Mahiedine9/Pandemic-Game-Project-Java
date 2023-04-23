b210a870c8d6e3369627c27f3919af8e046a76b9
mahiedine.ferdjoukh.etu <mahieddine.ferdjoukh9@gmail.com>
2023-04-23 02:43:48 +0200

    creation de la classe cure

---
+++ The Pandemic Game/src/pandemicgame/Cure.java
@@ -0,0 +1,11 @@
+public class Cure {
+    private Disease disease;
+
+    public Cure(Disease disease) {
+        this.disease = disease;
+    }
+
+    public Disease getDisease() {
+        return disease;
+    }
+}
