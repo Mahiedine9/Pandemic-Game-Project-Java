2d5d64d841be090502c6130520e6cd04a02facf8
Sid Ahmed FERROUDJ <sidahmed.ferroudj.etu@univ-lille.frgit config --global user.email sidahmed.ferroudj.etu@univ-lille.frgit config --global user.email sidahmed.ferroudj.etu@univ-lille.fr>
2023-05-25 01:07:54 +0200

    Le jeu s'initialise avec succès !

--- The Pandemic Game/src/pandemicgame/PandemicGameMain.java
+++ The Pandemic Game/src/pandemicgame/PandemicGameMain.java
@@ -1,89 +1,32 @@
 package pandemicgame;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList ;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PandemicGameMain {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		if (args.length != 1) {
-        System.out.println("Usage: java PandemicGameMain <JSON file path>");
-        return;
-        }
-		// création du plateau du jeu et de jeu
-		World myWorld = new World(args[0]) ;
-		// création du jeu
-		PandemicGame game = new PandemicGame(myWorld) ;
 		
-		
-		Scanner sc = new Scanner(System.in);
-        int nbPlayers = 0;
-        
-        while (nbPlayers < 1 || nbPlayers > 4) {
-        	System.out.println("Combien de joueurs voulez-vous créer ? (1 à 4 joueurs)");
-        	if (sc.hasNextInt()) {
-        		nbPlayers = sc.nextInt();
-        	} else {
-        		System.out.println("Veuillez entrer un nombre entre 1 et 4.");
-        		sc.next();
-        	}
-        }
-        
-        
-		
-		// sélection d'une ville
-		City city = myWorld.getCities().get(0) ;
-		
-		// création des joueurs
+		// créeation de jeu et des joeurs
+		// il s'agit d'une version initale de cette suite d'instructions preparatives
+		World myWorld = new World("The Pandemic Game/files/carte2.json") ;
 		ArrayList<Player> players = new ArrayList<Player>() ;
+		City ville = myWorld.getCities().get(0) ;
+        Doctor timo = new Doctor("timo", ville, myWorld) ;
+        Expert leon = new Expert("leon", ville, myWorld) ;
+        Globetrotter karim = new Globetrotter("karim", ville, myWorld) ;
+        Scientific lucas = new Scientific("lucas", ville, myWorld) ;
+        players.add(timo) ;
+        players.add(leon) ;
+        players.add(karim) ;
+        players.add(lucas) ;
+		PandemicGame game = new PandemicGame(myWorld, players) ;
 		
-		for (int i = 0; i < nbPlayers; i++) {
-			System.out.println("Création du joueur #" + (i+1));
-			System.out.println("Entrez le nom du joueur :");
-			String playerName = sc.next();
-			System.out.println("Entrez le rôle du joueur (doctor, scientist, expert, globetrotter) :");
-			String playerRole = sc.next();
-			
-			Player newPlayer = createPlayer(playerName, playerRole, city, myWorld);
-			
-			if (newPlayer != null) {
-				players.add(newPlayer);
-			} else {
-				System.out.println("Le rôle entré n'est pas valide.");
-				i--; // on décrémente i pour recréer un joueur
-			}
-		}
-		
-		sc.close();
-		
-		// initialisation du jeu
-		game.initiate() ;
-		System.out.print("Plteau de jeu a été crée. En ce moment il exist "+myWorld.getPlayerDeck().size()+" cartes joueuers (dont cartes épidimie) et "+myWorld.getInfectionDeck().size()+" cartes infection.") ;
-		System.out.println() ;
-		System.out.println() ;
-		
-	
-		//Tirge 5 cartes infection et infection des villes
-		for  (int i = 0 ; i<5 ; i++) {
-			InfectionCard card = myWorld.drawInfectionDeck() ;
-			Disease cardDisease = card.getDisease() ;
-			City  cardCity = card.getCity() ;
-			cardCity.addDisease(cardDisease) ;
-			System.out.println("carte infecition tirée => ville :"+cardCity.getName()+"   maladie :"+cardDisease+". la ville est désormais infectée") ;
-			
-		/**for (Player player : game.getPlayers()) {
-			PlayableCard carte1 = myWorld.drawPlayerDeck() ;
-			  if (carte1 instanceof PlayerCard) player.getHand().add((PlayerCard) carte1) ;
-			   System.out.println("CARTE EPIDIMIE TIREE !!! ") ;
-			PlayableCard carte2 = myWorld.drawPlayerDeck() ;
-			   if (carte1 instanceof PlayerCard) player.getHand().add((PlayerCard) carte2) ;
-			   System.out.println("CARTE EPIDIMIE TIREE !!! ") ;
-		}
-		*/
 
-			
-			
+		
+		game.initiate() ;
+	   	game.run() ;
 
 }
 		
@@ -97,5 +40,4 @@ public class PandemicGameMain {
 	
 	
 	
-	
-	}}
+	}
