b1c1939b80cde13699dd5301eae65ae6c45c3f29
Sid Ahmed FERROUDJ <sidahmed.ferroudj.etu@univ-lille.frgit config --global user.email sidahmed.ferroudj.etu@univ-lille.frgit config --global user.email sidahmed.ferroudj.etu@univ-lille.fr>
2023-05-29 11:43:38 +0200

    Code 100% documenté

--- The Pandemic Game/src/pandemicgame/PandemicGameMain.java
+++ The Pandemic Game/src/pandemicgame/PandemicGameMain.java
@@ -2,45 +2,34 @@ package pandemicgame;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class PandemicGameMain {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		// créeation de jeu et des joeurs
-		// il s'agit d'une version initale de cette suite d'instructions preparatives
-		World myWorld = new World("The Pandemic Game/files/carte2.json") ;
-		ArrayList<Player> players = new ArrayList<Player>() ;
-		City ville = myWorld.getCities().get(0) ;
-		Globetrotter karim = new Globetrotter("karim", ville, myWorld) ;
-        Doctor timo = new Doctor("timo", ville, myWorld) ;
-        Expert leon = new Expert("leon", ville, myWorld) ;
-        Scientific lucas = new Scientific("lucas", ville, myWorld) ;
-        players.add(timo) ;
-        players.add(leon) ;
-        players.add(karim) ;
-        players.add(lucas) ;
-		PandemicGame game = new PandemicGame(myWorld, players) ;
+		// Creating the game world and players
+		World myWorld = new World("The Pandemic Game/files/carte2.json");
+		ArrayList<Player> players = new ArrayList<Player>();
 		
-
-	  
-	    
+		// Selecting a city for the players to start
+		City ville = myWorld.getCities().get(0);
 		
-	    
+		// Creating player instances
+		Globetrotter karim = new Globetrotter("karim", ville, myWorld);
+        Doctor timo = new Doctor("timo", ville, myWorld);
+        Expert leon = new Expert("leon", ville, myWorld);
+        Scientific lucas = new Scientific("lucas", ville, myWorld);
+        
+        // Adding players to the list
+        players.add(timo);
+        players.add(leon);
+        players.add(karim);
+        players.add(lucas);
+        
+        // Creating the game instance
+		PandemicGame game = new PandemicGame(myWorld, players);
 		
-	    game.run() ;
-
-}
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		// Running the game
+		game.run();
 	}
+}
